cb352098dfe86068c3d834a6c60539d0da300bd8
alaahosny9 <alaahosnii1000@gmail.com>
2023-05-08 21:06:05 +0300

    completed sebha tab

--- lib/ui/home/sebha/sebha_tab.dart
+++ lib/ui/home/sebha/sebha_tab.dart
@@ -2,13 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
 
   @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int sebhaCount = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Center(
+      child: Column(
+        children: [
+          Image.asset('assets/images/sebha_header.png'),
+          Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: const Text(
+              'Sebha Count',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xFF242424),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            height: 75,
+            width: 65,
+            child: Card(
+              color: const Color(0xFFFB7935F),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Center(
+                child: Text(
+                  sebhaCount.toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF242424),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              minimumSize: MaterialStateProperty.all(Size.zero),
+            ),
+            onPressed: () {
+              sebhaCount++;
+              setState(() {});
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Sobhan Allah',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
